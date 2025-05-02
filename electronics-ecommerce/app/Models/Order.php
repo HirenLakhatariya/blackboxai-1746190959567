42e8a3b6fdc980645e101679a3408a33bcbed6f6
HirenLakhatariya <137195724+HirenLakhatariya@users.noreply.github.com>
2025-05-02 18:33:31 +0530

    Add electronics-ecommerce/app/Models/Order.php 1746191011324

---
+++ electronics-ecommerce/app/Models/Order.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Order extends Model
+{
+    /**
+     * Get the user that owns the order.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the order items for the order.
+     */
+    public function orderItems(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
